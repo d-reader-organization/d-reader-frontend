a9562614bc835a6a725afc01032101529563a25b
d-reader-josip <josip.volarevic@dreader.io>
2023-02-12 14:04:13 +0100

    chore: bump solomon and add mobile minting

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -6,42 +6,14 @@ import DiscordIcon from 'public/assets/vector-icons/discord-icon.svg'
 import TwitterIcon from 'public/assets/vector-icons/twitter-icon.svg'
 import SocialIcon from 'public/assets/vector-icons/social-icon.svg'
 import useAnchorElement from 'hooks/useAnchorElement'
-import { Web3MobileWallet } from '@solana-mobile/mobile-wallet-adapter-protocol-web3js'
-import { useAuth, useServerAuthorization, Account, removeAuthHeaders, lsRemoveWalletAuth } from '@open-sauce/solomon'
-import { SolanaMobileWalletAdapterWalletName } from '@solana-mobile/wallet-adapter-mobile'
-import { useWallet } from '@solana/wallet-adapter-react'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import http from 'api/http'
 
-const WalletMultiButtonDynamic = dynamic(
-	async () => (await import('@solana/wallet-adapter-material-ui')).WalletMultiButton,
-	{ ssr: false }
-)
-
-const MobileWalletMultiButtonDynamic = dynamic(
-	async () => await (await import('@open-sauce/solomon')).MobileWalletMultiButton,
-	{ ssr: false }
-)
+const WalletButtonDynamic = dynamic(async () => (await import('@open-sauce/solomon')).WalletButton, { ssr: false })
 
 const Navigation: React.FC<ToolbarProps> = (props) => {
-	const { setIsAuthenticated } = useAuth()
 	const [menuAnchorEl, setMenuAnchorEl, resetMenuAnchorEl] = useAnchorElement()
-	const { mobileConnect } = useServerAuthorization(http)
-	const { wallet } = useWallet()
-	const isMobileWallet = wallet?.adapter.name === SolanaMobileWalletAdapterWalletName
-
-	const onAuthorize = async (mobileWallet: Web3MobileWallet, account: Account) => {
-		// TODO: deprecate setIsAuthenticated(true)
-		setIsAuthenticated(true)
-		return await mobileConnect(mobileWallet, account)
-	}
-
-	const onDeauthorize = (account: Account) => {
-		removeAuthHeaders(http)
-		if (account?.address) lsRemoveWalletAuth(account.address)
-		// TODO: setIsAuthenticated(false)
-	}
 
 	return (
 		<Toolbar
@@ -115,15 +87,7 @@ const Navigation: React.FC<ToolbarProps> = (props) => {
 						<DiscordIcon />
 					</Button>
 				</Hidden>
-				{isMobileWallet ? (
-					<MobileWalletMultiButtonDynamic
-						className='wallet-button'
-						onAuthorize={onAuthorize}
-						onDeauthorize={onDeauthorize}
-					/>
-				) : (
-					<WalletMultiButtonDynamic variant='contained' className='wallet-button' />
-				)}
+				<WalletButtonDynamic http={http} className='wallet-button' />
 			</Box>
 		</Toolbar>
 	)
