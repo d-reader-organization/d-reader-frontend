e4bd9e84a9ff7d3e1c5e2195a5c246277acc87af
d-reader-josip <josip.volarevic@dreader.io>
2023-02-21 13:30:12 +0100

    chore: deprecate ComicReader component

--- components/ComicReader.tsx
+++ components/ComicReader.tsx
@@ -1,32 +1,34 @@
-import { Box } from '@mui/material'
-import { useFetchComicIssue } from 'api/comicIssue'
-import Image from 'next/image'
+// import { Box } from '@mui/material'
+// import { useFetchComicIssue } from 'api/comicIssue'
+// import Image from 'next/image'
 
 const ComicReader: React.FC = () => {
-	const { data: comicIssue } = useFetchComicIssue(1)
+	// const { data: comicIssue } = useFetchComicIssue(1)
 
-	if (!comicIssue) return null
+	// if (!comicIssue) return null
 
-	return (
-		<Box
-			onContextMenu={(e) => {
-				e.preventDefault()
-			}}
-			className='comic'
-		>
-			{comicIssue.pages?.map((page, i) => (
-				<Image
-					key={page.id}
-					src={page.image}
-					alt={`Page ${page.pageNumber}`}
-					width={900}
-					height={1200}
-					priority={i === 0}
-					className='comic-page'
-				/>
-			))}
-		</Box>
-	)
+	// return (
+	// 	<Box
+	// 		onContextMenu={(e) => {
+	// 			e.preventDefault()
+	// 		}}
+	// 		className='comic'
+	// 	>
+	// 		{comicIssue.pages?.map((page, i) => (
+	// 			<Image
+	// 				key={page.id}
+	// 				src={page.image}
+	// 				alt={`Page ${page.pageNumber}`}
+	// 				width={900}
+	// 				height={1200}
+	// 				priority={i === 0}
+	// 				className='comic-page'
+	// 			/>
+	// 		))}
+	// 	</Box>
+	// )
+
+	return null
 }
 
 export default ComicReader
