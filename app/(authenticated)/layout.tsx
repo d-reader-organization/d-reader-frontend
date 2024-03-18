21f9faddb7749a03e1c680fd55e8d4ede7173995
d-reader-josip <josip.volarevic@dreader.io>
2024-03-18 10:58:17 +0100

    chore: remove referral gatekeeping

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,11 +1,3 @@
-'use client'
-
-import { useFetchMe } from '@/api/user/queries/useFetchMe'
-import JoinTheBeta from '@/components/JoinTheBeta'
-
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
-	const { data: me } = useFetchMe()
-
-	if (me && !me.hasBetaAccess) return <JoinTheBeta />
 	return children
 }
