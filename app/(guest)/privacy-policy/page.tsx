c1ce168a7e1580d298f194ba14abf338e2a52b6f
d-reader-josip <josip.volarevic@dreader.io>
2024-04-03 13:37:17 +0200

    chore: update privacy policy

--- app/(guest)/privacy-policy/page.tsx
+++ app/(guest)/privacy-policy/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
 import { MDXRemote } from 'next-mdx-remote/rsc'
+
+export const metadata: Metadata = {
+	title: 'dReader - Privacy Policy',
+	description: '🔏 dReader Privacy Policy and Terms of Service',
+}
 
 export default async function RemoteMdxPage() {
 	// MDX text - can be from a local file, database, CMS, fetch, anywhere...
