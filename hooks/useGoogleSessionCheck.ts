30b7e86c002e8145901ee1a2090a9b0b0188c226
d-reader-luka <luka.crnogorac@dreader.io>
2024-04-25 16:44:58 +0200

    chore: trigger login with google only if user is not authenticated

--- hooks/useGoogleSessionCheck.ts
+++ hooks/useGoogleSessionCheck.ts
@@ -1,5 +1,6 @@
 import { useLoginGoogleUser } from '@/api/auth'
 import { RoutePath } from '@/enums/routePath'
+import { useUserAuth } from '@/providers/UserAuthProvider'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
@@ -8,9 +9,10 @@ export const useGoogleSessionCheck = () => {
 	const { push } = useRouter()
 	const { data: session } = useSession()
 	const { mutateAsync: loginWithGoogle } = useLoginGoogleUser()
+	const { isAuthenticated } = useUserAuth()
 
 	useEffect(() => {
-		if (session?.accessToken) {
+		if (session?.accessToken && !isAuthenticated) {
 			loginWithGoogle().then((value) => {
 				const shouldRegister = typeof value === 'boolean'
 				push(shouldRegister ? `${RoutePath.Register}?sso=google` : RoutePath.Home)
