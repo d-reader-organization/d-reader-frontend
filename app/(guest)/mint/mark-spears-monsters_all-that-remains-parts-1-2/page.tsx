4939531853fc8304b080b4b5c0a4cc04a7b20c70
athar <athar.mohammad@dreader.io>
2024-08-28 14:09:01 +0530

    chore: add mark-spear-monsters-all-that-remains metadata

---
+++ app/(guest)/mint/mark-spears-monsters_all-that-remains-parts-1-2/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MintPage from '../[id]/page'
+
+const TheMarkSpearsMonsterPage = () => {
+	return <MintPage params={{ id: '128' }} />
+}
+
+export default TheMarkSpearsMonsterPage
